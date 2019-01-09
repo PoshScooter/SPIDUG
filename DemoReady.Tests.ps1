3d3ab65819f25dfd0501c6431930c21823c90095
Scott Miller (IT - InfPulse) <Scott.Miller@dominos.com>
2019-01-09 09:06:41 -0500

    add domain checks

--- DemoReady.Tests.ps1
+++ DemoReady.Tests.ps1
@@ -9,4 +9,18 @@ Describe "Checking config" {
             $vm.ParentCheckpointName | Should -Match "DEMO READY"
         }
     }
+    Context "Domain checks" {
+        $locpwd = ConvertTo-SecureString "Password!" -AsPlainText -Force
+        $domCreds = New-Object System.Management.Automation.PSCredential ("poshscooter\administrator", $locpwd)
+        $x = Invoke-Command -VMName DC1 -Credential $domCreds { 
+            Get-ADComputer -Filter *
+        }
+        it "Domain should be not have S3 in it" {
+            ($x.DNSHostName.Where{ $_ -match 's3' }).count | should -be 0
+        }
+
+        it "Domain should only have 3 objects in it" {
+            $x.DNSHostName.count | should -be 3
+        }
+    }
 }
